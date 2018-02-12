d16123e29f8285cceb3a48146f5d5fe12dfb53c0
Bruno Pacheco <brunopacheco1@yahoo.com>
2018-02-12 21:08:02 +0100

    Initializing the project with a toy example.

---
+++ src/test/scala/com/dev/bruno/TextClassificationTest.scala
@@ -0,0 +1,16 @@
+package com.dev.bruno
+
+import org.junit._
+import Assert._
+
+@Test
+class TextClassificationTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
